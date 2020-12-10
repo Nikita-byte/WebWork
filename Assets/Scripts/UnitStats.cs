e419f7b99328e7c18d29c903e91b469f30709375
Nikita <rakiidut@gmail.com>
2020-12-10 16:13:44 +0300

    lesson3

---
+++ Assets/Scripts/UnitStats.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class UnitStats : NetworkBehaviour
+{
+
+    [SerializeField] int maxHealth;
+    [SyncVar] int _curHealth;
+
+    public Stat damage;
+
+    public int curHealth { get { return _curHealth; } }
+
+    public override void OnStartServer()
+    {
+        _curHealth = maxHealth;
+    }
+
+    public void SetHealthRate(float rate)
+    {
+        _curHealth = rate == 0 ? 0 : (int)(maxHealth / rate);
+    }
+}
